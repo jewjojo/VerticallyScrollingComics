763766e9ba4e07b93da4eebc38d2d0028cd65360
Joe Bernstein <jewjojo@terpmail.umd.edu>
2022-10-31 17:53:18 -0400

    layout files adjusted to support gridViews

--- app/src/main/java/com/umdproject/verticallyscrollingcomics/EditComicActivity.kt
+++ app/src/main/java/com/umdproject/verticallyscrollingcomics/EditComicActivity.kt
@@ -3,6 +3,7 @@ package com.umdproject.verticallyscrollingcomics
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.provider.FirebaseInitProvider
+import com.umdproject.verticallyscrollingcomics.databinding.EditComicActivityBinding
 import com.umdproject.verticallyscrollingcomics.databinding.HomePageBinding
 import com.umdproject.verticallyscrollingcomics.ui.fragments.AccountFragment
 import com.umdproject.verticallyscrollingcomics.ui.fragments.BrowseFragment
@@ -13,7 +14,7 @@ import com.umdproject.verticallyscrollingcomics.ui.fragments.CreateFragment
 class EditComicActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val binding = HomePageBinding.inflate(layoutInflater)
+        val binding = EditComicActivityBinding.inflate(layoutInflater)
         setContentView(binding.root)
         supportActionBar?.hide()
     }
