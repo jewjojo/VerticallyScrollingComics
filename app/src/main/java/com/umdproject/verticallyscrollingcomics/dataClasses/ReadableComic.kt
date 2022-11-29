863272da6a3158efd27ee1e4431bb0a2b995c846
Meehan Imam <meehan@wireless-10-104-107-151.umd.edu>
2022-11-29 11:05:23 -0500

    Added background color to readable comic class

--- app/src/main/java/com/umdproject/verticallyscrollingcomics/dataClasses/ReadableComic.kt
+++ app/src/main/java/com/umdproject/verticallyscrollingcomics/dataClasses/ReadableComic.kt
@@ -1,5 +1,7 @@
 package com.umdproject.verticallyscrollingcomics.dataClasses
 
+import android.graphics.Color
+
 data class ReadableComic (
     val id: String = "",
     val comicName: String = "",
@@ -10,5 +12,11 @@ data class ReadableComic (
     val thumbnailResId: String = "",
     val views: Int = 0,
     val ratingCount: Int = 0,
-    val ratingAverage: Int = 0
+    val ratingAverage: Int = 0,
+    val backgroundColor: Color = Color.valueOf(0F,0F,0F),
+    val scrollSpeed: Float = 0.5.toFloat(),
+    val panelOrder: MutableList<Int> = mutableListOf(),
+    val panelSpacing: Int = 1,
+    val panels: MutableList<ComicPanel> = mutableListOf(),
+    val commentList: MutableList<String>,
 )
